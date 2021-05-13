4e6ba0f0edcfb23938940cdc81826f3090d70f66
Guard-SK <81436587+Guard-SK@users.noreply.github.com>
2021-05-13 19:04:43 +0200

    heroku setup 3

--- bot.py
+++ bot.py
@@ -4,18 +4,18 @@ from discord.ext import commands
 import json
 import os
 
-if os.path.exists(os.getcwd() + "/config.json"):
-    with open("./config.json") as f:
-        configData = json.load(f)
+# if os.path.exists(os.getcwd() + "/config.json"):
+#     with open("./config.json") as f:
+#         configData = json.load(f)
 
-else:
-    configTemplate = {"Token": "", "Prefix": "Z"}
+# else:
+#     configTemplate = {"Token": "", "Prefix": "Z"}
 
-    with open(os.getcwd() + "/config.json", "w+") as f:
-        json.dump(configTemplate, f)
+#     with open(os.getcwd() + "/config.json", "w+") as f:
+#         json.dump(configTemplate, f)
 
-token = configData["Token"]
-prefix = configData["Prefix"]
+# token = configData["Token"]
+# prefix = configData["Prefix"]
 
 bot = commands.Bot(command_prefix="Z")
 
@@ -28,4 +28,4 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f"Pong {round(bot.latency*1000, 1)} ms!")
 
-bot.run(token)
+bot.run('DISCORD_TOKEN')
